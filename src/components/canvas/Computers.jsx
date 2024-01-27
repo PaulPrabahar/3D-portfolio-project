fb3542f4023d0b1657cde5fe5454f1f65c4eb35f
Paul Prabahar <01067771@aravind.org>
2024-01-27 10:11:17 +0530

    hero section and graaphic placement

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import {Suspense, useEffect, useState} from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+import CanvasLoader from '../Loader';
 
 const Computers = () => {
+  // const computer = useGLTF('./desktop_pc/scene.gltf')
   return (
-    <div>Computers</div>
-  )
-}
+   <div>1</div>
+  );
+};
 
-export default Computers
+const ComputersCanvas = () => {
+  return (
+  <div>2</div>
+  );
+};
+export default ComputersCanvas;
+
+// export default Computers
