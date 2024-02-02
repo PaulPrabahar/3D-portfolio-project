5243b43fa45a3ce5c0f4889f2a0696fc066257ee
Paul_Prabahar <paul.prabahar@aravind.org>
2024-02-02 14:20:42 +0530

    About section and section wrapper adder

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -42,26 +42,35 @@ const Navbar = () => {
 
         {/* mobile navbar */}
         <div className="sm:hidden flex flex-1 justify-end items-center">
-              <img src={toogle ? close : menu} alt="menu" className="w-[28px] h-[28px] object-contain cursor--pointer" onClick={() => setToogle(!toogle)} />
+          <img
+            src={toogle ? close : menu}
+            alt="menu"
+            className="w-[28px] h-[28px] object-contain cursor--pointer"
+            onClick={() => setToogle(!toogle)}
+          />
 
-              <div className={`${!toogle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
-              <ul className="list-none flex justify-end items-start flex-col gap-4">
-          {navLinks.map((link) => (
-            <li
-              key={link.id}
-              className={`${
-                active === link.title ? "text-white" : "text-secondary"
-              } font-poppins text-[16px] font-medium cursor-pointer`}
-              onClick={() => {
-                setToogle(!toogle);
-                setActive(link.title);
-              }}
-            >
-              <a href={`#${link.id}`}>{link.title}</a>
-            </li>
-          ))}
-        </ul>
-              </div>
+          <div
+            className={`${
+              !toogle ? "hidden" : "flex"
+            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+          >
+            <ul className="list-none flex justify-end items-start flex-col gap-4">
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className={`${
+                    active === link.title ? "text-white" : "text-secondary"
+                  } font-poppins text-[16px] font-medium cursor-pointer`}
+                  onClick={() => {
+                    setToogle(!toogle);
+                    setActive(link.title);
+                  }}
+                >
+                  <a href={`#${link.id}`}>{link.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
